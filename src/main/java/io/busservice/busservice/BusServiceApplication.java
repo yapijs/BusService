854bb6a365a54b954532cab29aa81b729a0423ef
yapijs <daviszarins90@gmail.com>
2022-12-08 20:55:05 +0200

    first commit

---
+++ src/main/java/io/busservice/busservice/BusServiceApplication.java
@@ -0,0 +1,13 @@
+package io.busservice.busservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BusServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/busservice/busservice/BusServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.busservice.busservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
